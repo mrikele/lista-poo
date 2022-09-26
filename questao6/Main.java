7c89fdfc6cb8f5eefbd7bfe70aef5a9bde6b1d5c
Maria Rikele <mariaarikele@gmail.com>
2022-09-26 17:34:03 -0300

    Questão 6 <alterada>

--- questao6/Main.java
+++ questao6/Main.java
@@ -2,10 +2,14 @@ package questao6;
 
 public class Main {
     public static void main(String[] args){
-        PersonIndividual personIndividual = new PersonIndividual("120.987.655-05");
-        PersonLegal personLegal = new PersonLegal( "123.456.789");    
+        PersonIndividual personIndividual = new PersonIndividual("130.987.655-05");
+        PersonLegal personLegal = new PersonLegal( "123.456.444");    
+        PersonIndividual personIndividual2 = new PersonIndividual("120.987.655-05");
+        PersonLegal personLegal2 = new PersonLegal( "093.456.129");    
         System.out.println(personIndividual);
         System.out.println(personLegal);
+        System.out.println(personIndividual2);
+        System.out.println(personLegal2);
     }
 }
         
